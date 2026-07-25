bc481a17a9f508173c0f62544f35513b6533c707
Wishduck <github@johnrosshiggins.com>
2026-07-24 19:23:15 -0700

    support dropping into tanks

--- src/item.cpp
+++ src/item.cpp
@@ -11639,8 +11639,9 @@ bool item::on_drop( const tripoint_bub_ms &pos, map &m )
         }
     }
 
-    // Prevent items with DESTROY_ON_DROP from being dropped
-    if( has_flag( flag_DESTROY_ON_DROP ) ) {
+    // Prevent items with DESTROY_ON_DROP from being dropped onto the ground
+    if( has_flag( flag_DESTROY_ON_DROP ) && ( !made_of( LIQUID ) ||
+            !m.has_flag( flag_LIQUIDCONT, pos ) ) ) {
         return true;
     }
 
