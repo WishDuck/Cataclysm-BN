1682c51f258d0872aaad6d05c2475d7db13717b4
Wishduck <github@johnrosshiggins.com>
2026-08-12 12:42:43 -0700

    I MISREAD I SCREM

--- src/enchantments/enchantment_value.cpp
+++ src/enchantments/enchantment_value.cpp
@@ -39,7 +39,8 @@ std::vector<enchantment_value_id> enchantment_value::define_child_enchantments(
         if (obj.has_array("suffixes")) {
             for (JsonObject jo : obj.get_array("suffixes")) {
                 auto temp = std::vector<enchantment_value_id>{suffixed.id};
-                res.insert_range(res.end(), suffixed.define_child_enchantments(main, temp, jo, false));
+                auto children = define_child_enchantments(main, temp, jo, false);
+                res.insert(res.end(), children.begin(), children.end());
             }
         } else {
             // End of the chain, this is the lowest node
@@ -65,8 +66,8 @@ void enchantment_value::load(const JsonObject& jo, const std::string& src) {
             last_enchantment_id_set.push_back(id);
             std::vector<enchantment_value_id> new_defined_id_set;
             for (const JsonObject& suffix : suffix_set) {
-                new_defined_id_set.insert_range( new_defined_id_set.end(),
-                    define_child_enchantments(*this, last_enchantment_id_set, suffix, first));
+                auto children = define_child_enchantments(*this, last_enchantment_id_set, suffix, first);
+                new_defined_id_set.insert( new_defined_id_set.end(), children.begin(), children.end());
             }
             last_enchantment_id_set = new_defined_id_set;
             first = false;
